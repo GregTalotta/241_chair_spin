052d867d24464f0cec139632e0aadae4628ea533
Gregory Talotta <gjtalotta@alaska.edu>
2021-04-16 20:34:02 -0900

    logic fix

--- host_code/chair.cs
+++ host_code/chair.cs
@@ -5,7 +5,6 @@ using System.IO.Ports;
 public class chair
 {
     SerialPort port = new SerialPort("COM7", 9600); //Set the board COM
-    
     // Start is called before the first update
     void Start()
     {
