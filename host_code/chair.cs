967e4759f56e4800dc78e8cdb0fce3e14a6c78dd
Gregory Talotta <gjtalotta@alaska.edu>
2021-04-20 18:16:37 -0900

    Final code

--- host_code/chair.cs
+++ host_code/chair.cs
@@ -1,41 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
-using System;
-using System.IO.Ports;
+// using System;
 using UnityEngine;
+using System.IO.Ports;
+using System.Threading;
 
-public class Chair
+public class Chair : MonoBehaviour
 {
-    SerialPort port = new SerialPort("COM3", 9600); //Set the board COM
+    public static SerialPort port = new SerialPort("COM3", 9600); //Set the board COM
     // Start is called before the first update
-    public void Start()
+    void Start()
     {
-        port.Open();
-        if (port !=null &&port.IsOpen)
-        {
-            port.Write("start");
-        }
+        OpenCom();
     }
 
-    // move is called once per update
-    public void Move(double angle)
-    {
-        if (port !=null &&port.IsOpen)
-        {
-            int true_angle = (int) angle + 90;
-            if((true_angle < 10)||(true_angle > 170)){
-                port.Write("a90"); // reset to midpoint
+    public void OpenCom(){
+        if(port != null){
+            port.DtrEnable = true;
+            port.RtsEnable = true;
+            if(port.IsOpen){
+                port.Close();
+                print("Closing port, because it's already open");
             }
-            port.Write("a" + true_angle);
+            else
+            {
+                port.Open();
+                port.ReadTimeout = 100;
+                port.Write("a90\n");
+            }
+        }
+        else{
+            if(port.IsOpen)
+            {
+                print("port is already open");
+            }
+            else{
+                print("port == null");
+            }
         }
     }
-
-    public void End()
+    // move is called once per update
+    public static void Move(int angle)
     {
-        if (port !=null &&port.IsOpen)
-        {
-            port.Write("end");
+        if(port.IsOpen){
+            int true_angle = (int) angle;
+            string sendstr = "a" + true_angle +"\n";
+            // if((true_angle < 10)||(true_angle > 170)){
+            //     sendstr = "a90\n"; // reset to midpoint
+            // }
+            print("moved: " +sendstr);
+            port.Write(sendstr);
         }
+        else{
+            if(port != null){
+                port.Open();
+                port.ReadTimeout = 100;
+                port.Write("a90\n");
+            }
+        }
+        // if (port !=null &&port.IsOpen)
+        // {
+        //     int true_angle = (int) angle + 90;
+        //     if((true_angle < 10)||(true_angle > 170)){
+        //         port.Write("a90"); // reset to midpoint
+        //     }
+        //     port.Write("a" + true_angle);
+        // }
+    }
+
+    void OnApplicationQuit(){
         port.Close();
     }
 }
